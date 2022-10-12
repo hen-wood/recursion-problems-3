378b94fbf7df5919f2b3f2db16703d1c2ddc6d8b
Jonathan Sarceno <johnny@Jonathans-MacBook-Air.local>
2022-10-12 13:42:34 -0700

    solve 12

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -13,8 +13,25 @@ isSorted([5, 4, 3, 2, 1]); // false
 
 function isSorted(arr) {
   // Your code here
+  debugger
+  let element = arr.pop()
+  debugger
+  if (element < arr[arr.length - 1]) {
+    debugger
+    return false
+
+  } else if (arr.length === 0){
+    debugger
+    return true
+
+  }
+  return isSorted(arr)
 }
 
+isSorted([1, 2, 3, 4, 5]); // true
+isSorted([1, 2, 4, 3, 5]); // false
+isSorted([2, 4, 6, 7, 8]); // true
+isSorted([5, 4, 3, 2, 1]); // false
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
